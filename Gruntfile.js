3074aa01fbb954eb94c3a68b1d7855f366be3d2e
Thomas B Homburg <tbh@grammatip.com>
2013-11-12 13:29:20 +0100

    Update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,7 @@ module.exports = function(grunt) {
 
     "use strict";
 
-    var path = require('path'),
-        lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
-        folderMount = function folderMount(connect, point) {
-            return connect.static(path.resolve(point));
-        };
+    var path = require('path');
 
     // load all grunt tasks
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
@@ -59,10 +55,7 @@ module.exports = function(grunt) {
                 options : {
                     port       : 9001,
                     hostname: 'localhost',
-                    base       : './build',
-                    middleware : function (connect, options) {
-                        return [lrSnippet, folderMount(connect, options.base)]
-                    }
+                    base       : './build'
                 }
             }
         },
